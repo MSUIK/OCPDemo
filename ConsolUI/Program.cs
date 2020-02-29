6a6f5da7eca734b0ad83ef4aaaa0f4df149e806a
Sufyan Iqbal <SUFYAN-IQBAL1111@outlook.com>
2020-03-01 03:20:40 +0500

    Add project files.

---
+++ ConsolUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsolUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          List<PersonModel> applicatins =new List<PersonModel>
+        }
+    }
+}
