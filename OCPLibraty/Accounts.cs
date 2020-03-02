e6957959c8b015be104670e6ba6ba025cabe05a4
Sufyan Iqbal <SUFYAN-IQBAL1111@outlook.com>
2020-03-02 15:27:23 +0500

    Sample of Demo Application Before Modifications

---
+++ OCPLibraty/Accounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCPLibraty
+{
+    public class Accounts
+    {
+        public EmployModel Create(PersonModel person)
+        {
+            EmployModel output = new EmployModel();
+            output.FirstName = person.FirstName;
+            output.LastName = person.LastName;
+            output.EmailAddress = $"{person.FirstName.Substring(1, 0)}{person.LastName}@acme.com";
+
+            return output;
+        }
+    }
+}
---
+++ OCPUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using OCPLibraty;
+using System;
+using System.Collections.Generic;
+
+namespace OCPUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicants = new List<PersonModel>
+            {
+                new PersonModel{FirstName="Tim", LastName= "Coery"},
+                new PersonModel{ FirstName="Sufyan", LastName="Iqbal"},
+                new PersonModel { FirstName="Arslan", LastName ="Ali"}
+            };
+
+            List<EmployModel> employees = new List<EmployModel>();
+            Accounts accountProcessor = new Accounts();
+            foreach(var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+            foreach(var emp in employees)
+            {
+                Console.WriteLine($"{ emp.FirstName}  {emp.LastName} : {emp.EmailAddress}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
